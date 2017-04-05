841aaa0039efbe10cf1e9e6f342c0d3fde8d92b5
leshary <lesha-ry@yandex.ru>
2017-04-06 00:03:33 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,27 +6,19 @@ using namespace std;
 struct key
 {
     string name;
-    string  number1;
+    string number1;
     string number2;
     string number3;
     string number4;
     string number5;
-    key (string name, string number1, string, string number3, string number4, string number5)
-    {
-        this->name = name;
-        this->number1 = number1;
-        this->number2 = number2;
-        this->number3 = number3;
-        this->number4 = number4;
-        this->number5 = number5;
-    }
+    key (string name, string const&number1 = string(), string const&number2 = string(), string const&number3 = string(), string const&number4 = string(), string const&number5  = string()): name(name), number1(number1), number2 (number2), number3 (number3), number4 (number4), number5 (number5) {}
 };
 class phone_book
 {
 private:
     set <key*> book;
 public:
-    void add_person (string name, string number1 , string number2 , string number3 , string number4 , string number5 )
+    void add_person ( string name, string const&number1 = string(), string const&number2 = string(), string const&number3 = string(), string const&number4 = string(), string const&number5  = string())
     {
         book.insert(new key ( name, number1, number2,  number3,  number4,  number5 ));
     }
@@ -44,30 +36,35 @@ public:
     }
     void prin_all ()
     {
-        for ( set<key*>::iterator it = book.begin(), s = book.end(); it!=s ;){
+        for ( set<key*>::iterator it = book.begin(); it!=book.end(); it++){
             cout<<(*it)->name<<" ";
-            if ((*it)->number1.empty() )
+            if ((*it)->number1.empty()!=true )
                 cout<<(*it)->number1<<" ";
-            if ((*it)->number2.empty() )
+            if ((*it)->number2.empty()!=true )
                 cout<<(*it)->number2<<" ";
-            if ((*it)->number3.empty() )
+            if ((*it)->number3.empty()!=true )
                 cout<<(*it)->number3<<" ";
-            if ((*it)->number4.empty() )
+            if ((*it)->number4.empty()!=true )
                 cout<<(*it)->number4<<" ";
-            if ((*it)->number5.empty() )
+            if ((*it)->number5.empty()!=true )
                 cout<<(*it)->number5;
             cout<<endl;
         }
-
-    }
+        }
+        phone_book () {}
+~phone_book ()
+        {
+           book.clear();
+        }
    };
 int main()
 {
     phone_book sk;
     sk.add_person("grisha",  "89117292300");
-    sk.add_person('artur',  '86543216546', '89348267524');
-    sk.add_person('dima', '84952147651', '880256275694', '887835468');
-    cout<<sk.find1('grisha')->number1<<endl;
-    sk.dell('artur');
+    sk.add_person("artur",  "86543216546", "89348267524");
+    sk.add_person("dima", "84952147651", "880256275694", "887835468");
+    cout<<sk.find1("grisha")->number1<<endl;
+    sk.dell("artur");
     sk.prin_all();
-}
+    return 0;
+    }
